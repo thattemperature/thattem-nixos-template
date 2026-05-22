bda397e7299393113289255c68bc4b0bd42890ac
thattemperature <2719023332@qq.com>
2026-05-22 14:45:56 +0800

    First commit.

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+
+  description = "Thattemperature's NixOS template";
+
+  inputs = {
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    thattem-nixpkgs-overlays = {
+      url = "github:thattemperature/thattem-nixpkgs-overlays";
+    };
+
+    thattem-nixos = {
+      url = "github:thattemperature/thattem-nixos-configurations";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        thattem-nixpkgs-overlays.follows = "thattem-nixpkgs-overlays";
+      };
+    };
+
+    thattem-home-manager = {
+      url = "github:thattemperature/thattem-home-manager-configurations";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+        thattem-nixpkgs-overlays.follows = "thattem-nixpkgs-overlays";
+      };
+    };
+
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      thattem-nixos,
+      thattem-home-manager,
+      ...
+    }:
+
+    {
+      nixosConfigurations = {
+
+        default = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            {
+              thattem.nixos.advanced.enable = true;
+              thattem.nixos.programming.enable = true;
+              thattem.nixos.type = "common";
+            }
+            thattem-nixos.nixosModules.default
+            thattem-home-manager.nixosModules.default
+          ];
+        };
+
+      };
+    };
+
+}
